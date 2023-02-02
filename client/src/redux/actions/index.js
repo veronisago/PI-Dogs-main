1ee927bbd9f5dcb651abfebf74d7da740f8e8391
Veronica <veronisago1999@gmail.com>
2023-02-02 16:31:51 -0500

    variables de entorno

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+const {
+    REACT_APP_BASE_URL
+  } = process.env;
 
 export const GET_ALL_DOGS = "GET_ALL_DOGS";
 export const GET_DOG_DETAIL = "GET_DOG_DETAIL";
@@ -9,7 +12,7 @@ export const LOADING = "LOADING";
 export function getDogs(params) {
     return async function (dispatch) {
         dispatch({ type: LOADING, payload: true })
-        var json = await axios.get('http://localhost:3001/dogs', { params })
+        var json = await axios.get(`${REACT_APP_BASE_URL}/dogs`, { params })
         dispatch({ type: GET_ALL_DOGS, payload: json.data })
         dispatch({ type: LOADING, payload: false })
     }
@@ -19,7 +22,7 @@ export function getDogs(params) {
 export function getDogDetail(id) {
     return async function (dispatch) {
         dispatch({ type: LOADING, payload: true })
-        var json = await axios.get(`http://localhost:3001/dogs/${id}`)
+        var json = await axios.get(`${REACT_APP_BASE_URL}/dogs/${id}`)
         dispatch({ type: GET_DOG_DETAIL, payload: json.data })
         dispatch({ type: LOADING, payload: false })
     }
@@ -27,14 +30,14 @@ export function getDogDetail(id) {
 
 export function postDog(payload) {
     return async function (dispatch) {
-        var response = await axios.post('http://localhost:3001/dogs', payload)
+        var response = await axios.post(`${REACT_APP_BASE_URL}/dogs`, payload)
         return response;
     }
 }
 
 export function getTemperaments() {
     return async function (dispatch) {
-        var json = await axios.get('http://localhost:3001/temperaments')
+        var json = await axios.get(`${REACT_APP_BASE_URL}/temperaments`)
         dispatch({ type: GET_TEMPERAMENTS, payload: json.data })
     }
 }
